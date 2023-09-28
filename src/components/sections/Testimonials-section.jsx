a263042e0992960150c814ede679b2967f25c606
Davi Job <davi.job@hotmail.com>
2023-09-28 13:32:20 -0300

    added testimonial section file

---
+++ src/components/sections/Testimonials-section.jsx
@@ -0,0 +1,15 @@
+function TestimonialsSection() {
+	return (
+		<section className="testimonials-section">
+			<div className="testimonials-section-container container">
+				<h2 className="testimonials-section-title heading-secondary">
+					Testimonials
+				</h2>
+
+				<div className="testimonial-card"></div>
+			</div>
+		</section>
+	);
+}
+
+export default TestimonialsSection;
