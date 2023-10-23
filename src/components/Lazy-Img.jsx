faa793dbb9da15a2328af8a483b95583b42c364a
Davi Job <davi.job@hotmail.com>
2023-10-23 12:16:57 -0300

    Update Lazy-Img.jsx

--- src/components/Lazy-Img.jsx
+++ src/components/Lazy-Img.jsx
@@ -1,20 +1,37 @@
+import { useEffect, useState } from "react";
+
 function LazyImg({ className, src, alt, hasSmall }) {
+	const [bgDisabled, setbgDisabled] = useState(false);
+
+	let smallSrc; // The src for the small version of the image
+
+	// When the image is loaded, add the loaded class to the parent and after a 175 delay remove the background image
 	function loaded({ target }) {
 		target.parentNode.classList.add("loaded");
 
 		setTimeout(() => {
-			target.parentNode.style.backgroundImage = "";
+			setbgDisabled(true);
+			console.log("bg removed");
 		}, 175);
 	}
 
-	let smallSrc;
+	// If the className is passed, check if the image is loaded, if not, enable the background image
+	useEffect(() => {
+		if (className) {
+			if (!className.includes("loaded")) {
+				setbgDisabled(false);
+				console.log("bg added");
+			}
+		}
+	}, [className]);
 
+	// If the image has a small version, use the img src to form the small img src
 	if (hasSmall) {
 		let commaIndex = src.lastIndexOf(".");
 
 		smallSrc = src.slice(0, commaIndex) + "-small";
 		smallSrc = smallSrc.replaceAll(`\\`, "/");
-		smallSrc = smallSrc.replace("/images", "/images/small");
+		smallSrc = smallSrc.replace("/images", "public/images/small");
 
 		smallSrc += ".webp";
 	}
@@ -23,13 +40,17 @@ function LazyImg({ className, src, alt, hasSmall }) {
 		<div
 			className={`lazy-img-box ${className ? className : ""}`}
 			style={
-				hasSmall
+				!bgDisabled && hasSmall
 					? {
 							backgroundImage: `url(${smallSrc})`,
 					  }
-					: {
+					: !hasSmall
+					? {
 							backgroundImage:
 								"linear-gradient( 135deg, rgba(189, 221, 241, 1) 0%, rgba(118, 168, 199, 1) 100%)",
+					  }
+					: {
+							backgroundImage: "",
 					  }
 			}
 		>
